008b7a35f6a8ada194f987a2db176da6fe2a605e
Keerthan <keerthanderla@gmail.com>
2025-06-17 14:49:18 +0530

    changes made to code

--- evenOrodd2.py
+++ evenOrodd2.py
@@ -9,3 +9,16 @@ def checkit(para):
 Values = [1,2,3]
 ans = checkit(Values)
 print(ans)
+
+#new code is added
+def checkit(para):
+    result = []
+    for element in para:
+        if element % 2 == 0:
+            result.append("Even")
+        else:
+            result.append("Odd")
+    return result
+Values = [1,2,3]
+ans = checkit(Values)
+print(ans)

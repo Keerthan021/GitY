b8c029d6b8bf9e66251f5404ce94b790fe7c4cef
Keerthan <keerthanderla@gmail.com>
2025-06-17 15:19:00 +0530

    some changes made y GitX

--- evenOrodd2.py
+++ evenOrodd2.py
@@ -34,7 +34,20 @@ def checkit(para):
 Values = [1,2,3]
 ans = checkit(Values)
 print(ans)
-# new code added by branch
+# new code added by branch, GitY
+def checkit(para):
+    result = []
+    for element in para:
+        if element % 2 == 0:
+            result.append("Even")
+        else:
+            result.append("Odd")
+    return result
+Values = [1,2,3]
+ans = checkit(Values)
+print(ans)
+
+# new code added by branch, GitX
 def checkit(para):
     result = []
     for element in para:

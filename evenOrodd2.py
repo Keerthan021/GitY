de2c5ae5aed9ba187e3f072d925c057728e3270e
Keerthan <keerthanderla@gmail.com>
2025-06-17 15:07:45 +0530

    changes made to branch

--- evenOrodd2.py
+++ evenOrodd2.py
@@ -34,3 +34,15 @@ def checkit(para):
 Values = [1,2,3]
 ans = checkit(Values)
 print(ans)
+# new code added by branch
+def checkit(para):
+    result = []
+    for element in para:
+        if element % 2 == 0:
+            result.append("Even")
+        else:
+            result.append("Odd")
+    return result
+Values = [1,2,3]
+ans = checkit(Values)
+print(ans)
